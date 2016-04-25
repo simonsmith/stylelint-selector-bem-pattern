796ad6cd9512884576e938ba1f18c9525e5b2e2e
David Clark <david.dave.clark@gmail.com>
2016-04-25 07:10:32 -0700

    Switch tester to stylelint-test-rule-tape

--- test/index.js
+++ test/index.js
@@ -1,63 +1,102 @@
-var ruleTester = require('stylelint-rule-tester');
+var testRule = require('stylelint-test-rule-tape');
 var selectorBemPattern = require('..');
 
 // Just a couple of quick tests to ensure postcss-bem-linter
 // is getting the hard work done
 
-var testRule = ruleTester(selectorBemPattern.rule, selectorBemPattern.ruleName);
+testRule(selectorBemPattern.rule, {
+  ruleName: selectorBemPattern.ruleName,
+  config: { preset: 'suit' },
+  skipBasicChecks: true,
 
-testRule({ preset: 'suit' }, function(tr) {
-  tr.ok('/** @define Foo */ .Foo {}');
-  tr.notOk('/** @define Foo */ .false {}', {
-    message: 'Invalid component selector ".false" (selector-bem-pattern)',
-    line: 1,
-    column: 20,
-  });
+  accept: [
+    { code: '/** @define Foo */ .Foo {}' },
+    { code: '/** @define Foo */ .Foo-bar {}' },
+  ],
 
-  tr.ok('/** @define Foo */ .Foo-bar {}');
-  tr.notOk('/** @define Foo */ .Foo_bar {}', {
-    message: 'Invalid component selector ".Foo_bar" (selector-bem-pattern)',
-    line: 1,
-    column: 20,
-  });
+  reject: [
+    {
+      code: '/** @define Foo */ .false {}',
+      message: 'Invalid component selector ".false" (selector-bem-pattern)',
+      line: 1,
+      column: 20,
+    },
+    {
+      code: '/** @define Foo */ .Foo_bar {}',
+      message: 'Invalid component selector ".Foo_bar" (selector-bem-pattern)',
+      line: 1,
+      column: 20,
+    },
+  ],
 });
 
-testRule({
-  componentName: '^[a-zA-Z]+$',
-  componentSelectors: '^\\.{componentName}---thing$',
-}, function(tr) {
-  tr.ok('/** @define Foo */\n  .Foo---thing {}');
-  tr.notOk('/** @define Foo2 */', {
-    message: 'Invalid component name in definition /*/** @define Foo2 */*/ (selector-bem-pattern)',
-    line: 1,
-    column: 1,
-  })
-  tr.notOk('/** @define Foo */\n  .Foo-thing {}', {
-    message: 'Invalid component selector ".Foo-thing" (selector-bem-pattern)',
-    line: 2,
-    column: 3,
-  });
-});
-
-testRule({
-  preset: 'suit',
-  presetOptions: {
-    namespace: 'qz',
+testRule(selectorBemPattern.rule, {
+  ruleName: selectorBemPattern.ruleName,
+  config: {
+    componentName: '^[a-zA-Z]+$',
+    componentSelectors: '^\\.{componentName}---thing$',
   },
-}, function(tr) {
-  tr.ok('/** @define Foo */\n  .qz-Foo--thing {}');
-  tr.notOk('/** @define Foo */\n  .Foo-thing {}', {
-    message: 'Invalid component selector ".Foo-thing" (selector-bem-pattern)',
-    line: 2,
-    column: 3,
-  });
-  tr.notOk('/** @define Foo */\n  .qz-Boooo-thing {}', {
-    message: 'Invalid component selector ".qz-Boooo-thing" (selector-bem-pattern)',
-    line: 2,
-    column: 3,
-  });
+  skipBasicChecks: true,
+
+  accept: [
+    { code: '/** @define Foo */\n  .Foo---thing {}' },
+  ],
+
+  reject: [
+    {
+      code: '/** @define Foo2 */',
+      message: 'Invalid component name in definition /*/** @define Foo2 */*/ (selector-bem-pattern)',
+      line: 1,
+      column: 1,
+    },
+    {
+      code: '/** @define Foo */\n  .Foo-thing {}',
+      message: 'Invalid component selector ".Foo-thing" (selector-bem-pattern)',
+      line: 2,
+      column: 3,
+    },
+  ],
 });
 
-testRule(null, function(tr) {
-  tr.notOk('a {}', 'Expected option value for rule "selector-bem-pattern"');
+testRule(selectorBemPattern.rule, {
+  ruleName: selectorBemPattern.ruleName,
+  config: {
+    preset: 'suit',
+    presetOptions: {
+      namespace: 'qz',
+    },
+  },
+  skipBasicChecks: true,
+
+  accept: [
+    { code: '/** @define Foo */\n  .qz-Foo--thing {}' },
+  ],
+
+  reject: [
+    {
+      code: '/** @define Foo */\n  .Foo-thing {}',
+      message: 'Invalid component selector ".Foo-thing" (selector-bem-pattern)',
+      line: 2,
+      column: 3,
+    },
+    {
+      code: '/** @define Foo */\n  .qz-Boooo-thing {}',
+      message: 'Invalid component selector ".qz-Boooo-thing" (selector-bem-pattern)',
+      line: 2,
+      column: 3,
+    },
+  ],
+});
+
+testRule(selectorBemPattern.rule, {
+  ruleName: selectorBemPattern.ruleName,
+  config: null,
+  skipBasicChecks: true,
+
+  reject: [
+    {
+      code: 'a {}',
+      message: 'Expected option value for rule "selector-bem-pattern"',
+    },
+  ],
 });
